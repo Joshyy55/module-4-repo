6b1b617a0566fb832ab8a1b59281e8e1887d05b7
joshcool55🎮 Anthony <urrutiajosh55@gmail.com>
2024-03-31 00:40:55 -0700

    setting up first page function

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleSubmit() {
+    var user = document.getElementById("username").value;
+    var title = document.getElementById("title").value;
+    var cont = document.getElementById("content").value;
+}
