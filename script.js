dac4ea015b949e686f006e04c17e8c557e965e03
joshcool55🎮 Anthony <urrutiajosh55@gmail.com>
2024-04-02 01:41:55 -0700

    touching it up

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
 function handleSubmit() {
-
     var user = document.getElementById("username").value;
     var title = document.getElementById("title").value;
     var cont = document.getElementById("content").value;
+
     if (user && title && cont) {
         var pageOne = document.querySelector(".pageOne");
         pageOne.classList.add("hide");
         var pageTwo = document.querySelector(".pageTwo");
         pageTwo.classList.remove("hide");
 
+        var blog = document.createElement("div");
+        blog.setAttribute("class", "blog");
+        var text = document.createTextNode(`${title} created by ${user}: ${cont}`);
+        blog.appendChild(text);
+        var blogList = document.querySelector(".blogs");
+        blogList.appendChild(blog);
     } else {
-        alert("please fill everything out in the page pretty please")
+        alert("Please fill out all on this page pretty please.");
     }
-    var blog = document.createElement("div");
-    
-    
 }
 
 function handleBack() {
@@ -29,4 +32,26 @@ function handleBack() {
     user.value = "";
     title.value = "";
     cont.value = "";
+}
+let toggle = true
+function colorChange() {
+
+    if (toggle === true) {
+        var buton = document.querySelector(".dayNight")
+        buton.textContent = "🌝"
+        var theme = document.querySelector(".background")
+        theme.style.backgroundColor = "black"
+        theme.style.color = "white"
+        toggle = false
+    } else if (toggle === false) {
+        var buton = document.querySelector(".dayNight")
+        buton.textContent = "🌚"
+        var theme = document.querySelector(".background")
+        theme.style.backgroundColor = "white"
+        theme.style.color = "black"
+        toggle = true
+    }
+
+
+
 }
