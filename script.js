084bb5fa0a9def18d8833510f52c432eecbe4e5a
joshcool55🎮 Anthony <urrutiajosh55@gmail.com>
2024-04-02 01:54:00 -0700

    local storage

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function handleSubmit() {
 
         var blog = document.createElement("div");
         blog.setAttribute("class", "blog");
-        var text = document.createTextNode(`${title} created by ${user}: ${cont}`);
+        var text = document.createTextNode(`${title} ${user}: ${cont}`);
         blog.appendChild(text);
         var blogList = document.querySelector(".blogs");
         blogList.appendChild(blog);
@@ -54,4 +54,44 @@ function colorChange() {
 
 
 
+}
+function handleSubmit() {
+    var user = document.getElementById("username").value;
+    var title = document.getElementById("title").value;
+    var cont = document.getElementById("content").value;
+
+    if (user && title && cont) {
+        // Create blog post object
+        var post = {
+            username: user,
+            title: title,
+            content: cont
+        };
+
+        // Retrieve existing posts from localStorage
+        var existingPosts = localStorage.getItem('posts');
+        existingPosts = existingPosts ? JSON.parse(existingPosts) : [];
+
+        // Add the new post to the existing posts array
+        existingPosts.push(post);
+
+        // Save the updated posts array back to localStorage
+        localStorage.setItem('posts', JSON.stringify(existingPosts));
+
+        // Hide page one and show page two
+        var pageOne = document.querySelector(".pageOne");
+        pageOne.classList.add("hide");
+        var pageTwo = document.querySelector(".pageTwo");
+        pageTwo.classList.remove("hide");
+
+        // Create blog post element and append to list
+        var blog = document.createElement("div");
+        blog.setAttribute("class", "blog");
+        var text = document.createTextNode(`${title} created by ${user}: ${cont}`);
+        blog.appendChild(text);
+        var blogList = document.querySelector(".blogs");
+        blogList.appendChild(blog);
+    } else {
+        alert("Please fill out all fields on this page.");
+    }
 }
